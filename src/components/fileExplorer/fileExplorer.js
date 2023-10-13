fb18c730840256127d56b0f44744e5e83e5fbb2d
thisiskamaljoshi <thisiskamaljoshi@gmail.com>
2023-10-13 23:21:52 +0530

    Create Functionality Added

--- src/components/fileExplorer/fileExplorer.js
+++ src/components/fileExplorer/fileExplorer.js
@@ -71,6 +71,52 @@ const starterFiles = [
 const FileExplorer = () => {
   const [fileExplorer, setFileExplorer] = useState(starterFiles);
 
+  // Recursive handler for adding a new file or folder
+  const addFileHandler = (arr, parentKey, newName, isFolder) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].key === parentKey) {
+        // Create a new file or folder object
+        const newFile = {
+          key: `${parentKey}.${arr[i].children.length}`,
+          name: newName,
+          isFolder: isFolder,
+          children: isFolder ? [] : undefined, // If it's a folder, initialize children as an empty array
+        };
+
+        // Add the new file or folder to the parent's children
+        arr[i].children = [...(arr[i].children || []), newFile];
+        return true; // Element added successfully
+      }
+
+      if (arr[i].children && arr[i].children.length > 0) {
+        // Recursively check children
+        const childAdded = addFileHandler(arr[i].children, parentKey, newName, isFolder);
+        if (childAdded) {
+          return true; // Stop further searching if the element is added
+        }
+      }
+    }
+
+    return false; // Parent element not found
+  };
+
+  const addNewFolder = (folder,newName= "NewFolder",isFolder= true,event) => {
+
+    
+
+    event.preventDefault(); // to stop toggle event from running
+
+    let folderNameInput = prompt('Type folder/file name');
+
+    console.log(event,"event",folderNameInput)
+
+    setFileExplorer((prev) => {
+      const newState = JSON.parse(JSON.stringify(prev));
+      addFileHandler(newState, folder.key,folderNameInput ?? newName, isFolder);
+      return newState;
+    });
+  };
+
    const deleteElementByKey = (arr, targetKey) => {
     for (let i = 0; i < arr.length; i++) {
       if (arr[i].key === targetKey) {
@@ -120,6 +166,8 @@ const FileExplorer = () => {
         >
           {folder.name}
           <span className={styles.deleteHandler} onClick={() => fileNFolderDeleteHandler(folder)}><Delete/></span>
+          <span className={styles.deleteHandler} onClick={(event) => addNewFolder(folder,"NewFolder",true,event)}> AF</span>
+          <span className={styles.deleteHandler} onClick={(event) => addNewFolder(folder,"NewFile",false,event)}> F</span>
         </label>
         <div className={styles.explorerFolderContent}>
           {fileExplorerFileHandler(folder.children)}

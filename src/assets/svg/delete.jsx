5c5daadf045c4c367f46d05bf4d21389ae88eb31
thisiskamaljoshi <thisiskamaljoshi@gmail.com>
2023-10-13 22:29:50 +0530

    Delete Functionality Added

---
+++ src/assets/svg/delete.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="15"
+      height="15"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="#ccc"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M10 11v6M14 11v6M4 7h16M6 7h12v11a3 3 0 01-3 3H9a3 3 0 01-3-3V7zM9 5a2 2 0 012-2h2a2 2 0 012 2v2H9V5z"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
--- src/components/fileExplorer/fileExplorer.js
+++ src/components/fileExplorer/fileExplorer.js
@@ -1,43 +1,62 @@
 import React, { useState } from "react";
 import styles from "./fileExplorer.module.scss";
 
+import Delete from "../../assets/svg/delete"
+
 const starterFiles = [
   {
-    key:"1",
+    key: "0",
     name: "Meta",
     isFolder: true,
     children: [
       {
-        key:"1.1",
+        key: "0.0",
         name: "Instagram",
-        children: [{key:"1.1.1", name: "Threads", children: [], isFolder: false }],
+        children: [
+          { key: "0.0.0", name: "Threads", children: [], isFolder: false },
+        ],
         isFolder: true,
       },
       {
-        key:"1.2",
+        key: "0.1",
         name: "Facebook",
         children: [
           {
-            key:"1.2.1",
+            key: "0.1.0",
             name: "Marketplace",
             children: [
-              { key:"1.2.1.1", name: "Vendors", children: [],isFolder:true },
-              { key:"1.2.1.2", name: "Users", children: [],isFolder:false },
-            ]
-            ,isFolder:true
+              { key: "0.1.0.0", name: "Vendors", children: [], isFolder: true },
+              { key: "0.1.0.1", name: "Users", children: [], isFolder: false },
+            ],
+            isFolder: true,
           },
           {
-            key:"1.2.2",
+            key: "0.1.1",
             name: "Social App",
             isFolder: false,
             children: [
               {
                 name: "Person",
-                key:"1.2.2.1",
+                key: "0.1.1.0",
                 children: [
-                  { key:"1.2.2.1.1", name: "Friends", children: [],isFolder:true },
-                  { key:"1.2.2.1.2", name: "Posts", children: [],isFolder:false },
-                  { key:"1.2.2.1.3",name: "Videos", children: [],isFolder:false },
+                  {
+                    key: "0.1.1.0.0",
+                    name: "Friends",
+                    children: [],
+                    isFolder: true,
+                  },
+                  {
+                    key: "0.1.1.0.1",
+                    name: "Posts",
+                    children: [],
+                    isFolder: false,
+                  },
+                  {
+                    key: "0.1.1.0.2",
+                    name: "Videos",
+                    children: [],
+                    isFolder: false,
+                  },
                 ],
               },
             ],
@@ -49,46 +68,103 @@ const starterFiles = [
   },
 ];
 
-const fileExplorerFolderHandler = (folder) => {
-  return <>
-    <input className={styles.hiddenCheckBox} type="checkbox" id={folder.key+folder.name} name={folder.key+folder.name}/>
-    <label htmlFor={folder.key+folder.name} className={styles.explorerFolder}>{folder.name}</label>
-    <div className={styles.explorerFolderContent}>
-        {fileExplorerFileHandler(folder.children)}
-    </div>
-    
-  </>
-    
-};
-
-const fileExplorerFileHandler = (files) => {
-  return (
-    <>
-      {files.map((file) => {
-        if(file.isFolder){
-            //i.e it is a folder
-            return <div key={file.key}>{fileExplorerFolderHandler(file)}</div> 
-        }
-        return  <p className={styles.explorerFile} key={file.key}>{file.name}</p>
-      })}
-    </>
-  );
-};
-
 const FileExplorer = () => {
-  const [fileExplorer, setfileExplorer] = useState(starterFiles);
+  const [fileExplorer, setFileExplorer] = useState(starterFiles);
+
+   const deleteElementByKey = (arr, targetKey) => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].key === targetKey) {
+        arr.splice(i, 1);
+        return true; // Element found and deleted
+      }
+
+      if (arr[i].children && arr[i].children.length > 0) {
+        // Recursively check children
+        const childDeleted = deleteElementByKey(arr[i].children, targetKey);
+
+        if (childDeleted && arr[i].children.length === 0) {
+          // If the last child was deleted, remove the 'children' property
+          delete arr[i].children;
+        }
+
+        if (childDeleted) {
+          return true; // Stop further searching if the element is found and deleted
+        }
+      }
+    }
+
+    return false; // Element not found
+  };
+
+    const fileNFolderDeleteHandler = (fileNfolder) => {
+    setFileExplorer((prev) => {
+      // Create a deep copy of the state before modifying it
+      const newState = JSON.parse(JSON.stringify(prev));
+      deleteElementByKey(newState, fileNfolder.key);
+      return newState;
+    });
+  };
+
+  const fileExplorerFolderHandler = (folder) => {
+    return (
+      <>
+        <input
+          className={styles.hiddenCheckBox}
+          type="checkbox"
+          id={folder.key + folder.name}
+          name={folder.key + folder.name}
+        />
+        <label
+          htmlFor={folder.key + folder.name}
+          className={styles.explorerFolder}
+        >
+          {folder.name}
+          <span className={styles.deleteHandler} onClick={() => fileNFolderDeleteHandler(folder)}><Delete/></span>
+        </label>
+        <div className={styles.explorerFolderContent}>
+          {fileExplorerFileHandler(folder.children)}
+        </div>
+      </>
+    );
+  };
+
+  const fileExplorerFileHandler = (files) => {
+    return (
+      <>
+        {files?.map((file) => {
+          if (file.isFolder) {
+            
+            return <div key={file.key}>{fileExplorerFolderHandler(file)} </div>;
+          }
+          return (
+            <p className={styles.explorerFile} key={file.key}>
+              {file.name}{" "}
+              <span className={styles.deleteHandler} onClick={() => fileNFolderDeleteHandler(file)}><Delete/></span>
+            </p>
+          );
+        })}
+      </>
+    );
+  };
+
   return (
     <div className={styles.fileExplorer}>
       <div className={styles.fileMain}>
-        {fileExplorer.map((fileNFolder) => {
-           if (fileNFolder.isFolder) {
-            //i.e it is a folder
-            return <div key={fileNFolder.key}>{fileExplorerFolderHandler(fileNFolder)}</div> 
-             
+        {Array.isArray(fileExplorer) && fileExplorer?.map((fileNFolder) => {
+          if (fileNFolder.isFolder) {
+            
+            return (
+              <div key={fileNFolder.key}>
+                {fileExplorerFolderHandler(fileNFolder)}
+              </div>
+            );
           }
-          return <div className={styles.explorerFolderContent} key={fileNFolder.key}>
-                {fileExplorerFileHandler(fileNFolder.children)}
-    </div> 
+          return (
+            <div className={styles.explorerFolderContent} key={fileNFolder.key}>
+              {fileExplorerFileHandler(fileNFolder.children)}
+              <span className={styles.deleteHandler} onClick={() => fileNFolderDeleteHandler(fileNFolder)}><Delete/></span>
+            </div>
+          );
         })}
       </div>
     </div>

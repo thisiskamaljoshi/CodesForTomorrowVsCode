4b793672b58ad261a4229ae8643efe1d3fb12e34
thisiskamaljoshi <thisiskamaljoshi@gmail.com>
2023-10-13 20:06:01 +0530

    Key Issue Resolved

--- src/components/fileExplorer/fileExplorer.js
+++ src/components/fileExplorer/fileExplorer.js
@@ -10,7 +10,7 @@ const starterFiles = [
       {
         key:"1.1",
         name: "Instagram",
-        children: [{ name: "Threads", children: [], isFolder: false }],
+        children: [{key:"1.1.1", name: "Threads", children: [], isFolder: false }],
         isFolder: true,
       },
       {
@@ -33,10 +33,11 @@ const starterFiles = [
             children: [
               {
                 name: "Person",
+                key:"1.2.2.1",
                 children: [
-                  { key:"1.2.2.1", name: "Friends", children: [],isFolder:true },
-                  { key:"1.2.2.2", name: "Posts", children: [],isFolder:false },
-                  { key:"1.2.2.3",name: "Videos", children: [],isFolder:false },
+                  { key:"1.2.2.1.1", name: "Friends", children: [],isFolder:true },
+                  { key:"1.2.2.1.2", name: "Posts", children: [],isFolder:false },
+                  { key:"1.2.2.1.3",name: "Videos", children: [],isFolder:false },
                 ],
               },
             ],
@@ -51,7 +52,7 @@ const starterFiles = [
 const fileExplorerFolderHandler = (folder) => {
   return <>
     <input className={styles.hiddenCheckBox} type="checkbox" id={folder.key+folder.name} name={folder.key+folder.name}/>
-    <label for={folder.key+folder.name} className={styles.explorerFolder}>{folder.name}</label>
+    <label htmlFor={folder.key+folder.name} className={styles.explorerFolder}>{folder.name}</label>
     <div className={styles.explorerFolderContent}>
         {fileExplorerFileHandler(folder.children)}
     </div>
@@ -66,9 +67,9 @@ const fileExplorerFileHandler = (files) => {
       {files.map((file) => {
         if(file.isFolder){
             //i.e it is a folder
-            return fileExplorerFolderHandler(file)
+            return <div key={file.key}>{fileExplorerFolderHandler(file)}</div> 
         }
-        return  <p className={styles.explorerFile}>{file.name}</p>
+        return  <p className={styles.explorerFile} key={file.key}>{file.name}</p>
       })}
     </>
   );
@@ -82,13 +83,11 @@ const FileExplorer = () => {
         {fileExplorer.map((fileNFolder) => {
            if (fileNFolder.isFolder) {
             //i.e it is a folder
-            return <>
-            {fileExplorerFolderHandler(fileNFolder)}
-            
-            </>
+            return <div key={fileNFolder.key}>{fileExplorerFolderHandler(fileNFolder)}</div> 
+             
           }
-          return <div className={styles.explorerFolderContent}>
-        fileExplorerFileHandler(fileNFolder.children);
+          return <div className={styles.explorerFolderContent} key={fileNFolder.key}>
+                {fileExplorerFileHandler(fileNFolder.children)}
     </div> 
         })}
       </div>
